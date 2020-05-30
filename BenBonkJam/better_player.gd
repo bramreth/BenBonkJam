00c732033d84280da4e0a0a25c2327624af65f36
bram <bramreth@gmail.com>
2020-05-30 01:27:27 +0100

    more thumper tweaks

--- BenBonkJam/better_player.gd
+++ BenBonkJam/better_player.gd
@@ -11,19 +11,18 @@ func shoot():
 	b.global_position = $body/head/gun/origin.global_position
 	get_tree().get_root().add_child(b)
 	$body/head/gun/origin/flash.restart()
-	
-	$body/head/gun/Timer.start()
+	$body/head/gun/AnimationPlayer.play("reload")
 	cam.add_trauma(0.15)
 	
 func _physics_process(delta):
 	if dead: return
 	if Input.get_action_strength("click") and can_shoot:
 		shoot()
-		$body/head/gun/CurveTween.play(0.2, $body/head/gun.offset.x, $body/head/gun.offset.x - 25)
-
-func _on_Timer_timeout():
-	can_shoot = true
 
 
 func _on_CurveTween_curve_tween(sat):
-	$body/head/gun.offset.x = sat
+	$body/head/gun.position.x = sat
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	can_shoot = true
